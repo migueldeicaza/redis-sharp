de76ed275d7e15cb15f82e853315e933ce99420c
Jacob Lewallen <jlewalle@gmail.com>
2010-03-01 21:28:33 -0800

    Sort command, first pass. Hey, it works. Redis is too simple.

--- redis-sharp.cs
+++ redis-sharp.cs
@@ -165,6 +165,11 @@ public class Redis : IDisposable {
 		return Encoding.UTF8.GetString (Get (key));
 	}
 
+  public byte[][] Sort(SortOptions options)
+  {
+    return SendDataCommandExpectMultiBulkReply(null, options.ToCommand() + "\r\n");
+  }
+
 	public byte [] GetSet (string key, byte [] value)
 	{
 		if (key == null)
@@ -708,4 +713,32 @@ public class Redis : IDisposable {
 		}
 	}
 }
+
+  public class SortOptions
+  {
+    public string Key { get; set; }
+    public bool Descending { get; set; }
+    public bool Lexographically { get; set; }
+    public Int32 LowerLimit { get; set; }
+    public Int32 UpperLimit { get; set; }
+    public string By { get; set; }
+    public string StoreInKey { get; set; }
+    public string Get { get; set; }
+
+    public string ToCommand()
+    {
+      var command = "SORT " + this.Key;
+      if (LowerLimit != 0 || UpperLimit != 0)
+        command += " LIMIT " + LowerLimit + " " + UpperLimit;
+      if (Lexographically)
+        command += " ALPHA";
+      if (!string.IsNullOrEmpty(By))
+        command += " BY " + By;
+      if (!string.IsNullOrEmpty(Get))
+        command += " GET " + Get;
+      if (!string.IsNullOrEmpty(StoreInKey))
+        command += " STORE " + StoreInKey;
+      return command;
+    }
+  }
 
